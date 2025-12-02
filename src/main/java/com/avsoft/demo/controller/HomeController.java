1cef5586f9e346334cc9d9318bb742f34f256085
Ajay63033 <ajaykatamreddi8524@gmail.com>
2025-12-02 21:46:29 +0530

    Initial commit

---
+++ src/main/java/com/avsoft/demo/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.avsoft.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello";
+    }
+
+    @GetMapping("/home")
+    public String home() {
+        return "home";
+    }
+   
+}
---
+++ src/test/java/com/avsoft/CustomeLoginAndSignup/CustomeLoginAndSignupApplicationTests.java
@@ -0,0 +1,14 @@
+package com.avsoft.CustomeLoginAndSignup;
+
+import com.avsoft.demo.CustomeLoginAndSignupApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = CustomeLoginAndSignupApplication.class)
+class CustomeLoginAndSignupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
